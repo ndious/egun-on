4cb98d3825d3c443b00946e25465351c5649726a
Nicolas Dufreche <n.dufreche@gmail.com>
2017-06-08 10:55:48 +0200

    template fixes

--- src/components/home/container/home.js
+++ src/components/home/container/home.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router'
+import routeFor from 'react-route-config'
+import TiCamera from 'react-icons/lib/ti/camera'
 
+const iconStyle = {
+  height: '125px',
+  width: '100%',
+  position: 'fixed',
+  marginLeft: '-15px',
+  bottom: '10px',
+}
 
 const Home = () => (<span>
   <h1>Egun on <small>Bonjour</small></h1>
@@ -11,6 +21,7 @@ const HomeBlock = ({ is_searching }) => {
   return (
     <span>
       {!is_searching && <Home />}
+      <Link to={routeFor('qr-code')}><TiCamera style={iconStyle} /></Link>
     </span>
   )
 }
--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,19 +1,9 @@
 import React from 'react'
 import { Container } from 'reactstrap'
-import { Link } from 'react-router'
-import routeFor from 'react-route-config'
-import TiCamera from 'react-icons/lib/ti/camera'
 
 import Search from './container/search'
 import Result from './container/result'
 import HomeBlock from './container/home'
-
-const iconStyle = {
-  height: '200px',
-  width: '100%',
-  position: 'absolute',
-  bottom: '10px',
-}
 
 const Home = () => {
   console.scope('pages:home')
@@ -25,7 +15,6 @@ const Home = () => {
         <Result />
         <HomeBlock />
       </Container>
-      <Link to={routeFor('qr-code')}><TiCamera style={iconStyle} /></Link>
     </span>
   )
 }
